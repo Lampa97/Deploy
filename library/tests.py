21bbb6a14973007bf9b199e61261674b47c57517
Roman.Iastremskyi <iastremskyi010397@gmail.com>
2025-04-09 09:58:57 +0300

    added linters

--- library/tests.py
+++ library/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Author, Book
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+    def setUp(self):
+        self.author = Author.objects.create(
+            first_name="John", last_name="Doe", birth_date="1980-01-01"
+        )
+        self.book = Book.objects.create(
+            title="Test Book", publication_date="1800-06-06", author=self.author
+        )
+
+    def test_author_str(self):
+        self.assertEqual(str(self.author), "John Doe")
+
+    def test_book_str(self):
+        self.assertEqual(str(self.book), "Test Book")
